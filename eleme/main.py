fbe8fe4e10ccf4bd4fed9a25d89f52b845e89a03
Monika <monikakotula2019@gmail.com>
2022-05-05 21:03:19 +0200

    day3

---
+++ eleme/main.py
@@ -0,0 +1,32 @@
+# row = 5
+#
+# for i in range (1,row+1):
+#     for j in range(1, i+1):
+#         print(j,end='')
+#     print('')
+
+# s = 0
+# # number = int(input('wpisz liczbę '))
+# # for i in range (1, number+1):
+# #     s+= i
+# # print(s)
+# #
+# # suma = sum(range(1, number+1))
+# # print(suma)
+
+# number = int(input('wpisz liczbę '))
+# for i in range(1, 11):
+#     wynik = i*number
+#     print(wynik)
+
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+#
+# for i in numbers:
+#     if i > 500:
+#         break
+#     elif i > 150:
+#         continue
+#     elif i % 5 == 0:
+#         print(i)
+
+
